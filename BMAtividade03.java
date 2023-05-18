2738b61f2344f2185fda3dcd491299932d22377e
Israel Oliveira <israel.oliveira@academico.ifpb.edu.br>
2023-05-18 10:25:25 -0300

    Terminado CBB E BM

---
+++ BMAtividade03.java
@@ -0,0 +1,16 @@
+public class BMAtividade03 {
+   
+    public static double metrosQuadradosParaPesQuadrados(double metrosQuadrados) {
+        return metrosQuadrados * 10.76;
+    }
+    public static double pesQuadradosParaCentimetrosQuadrados(double pesQuadrados) {
+        return pesQuadrados * 929;
+    }
+    
+    public static double milhasQuadradasParaAcres(double milhasQuadradas) {
+        return milhasQuadradas * 640;
+    }
+    public static double acresParaPesQuadrados(double acres) {
+        return acres * 43560;
+    }
+}
